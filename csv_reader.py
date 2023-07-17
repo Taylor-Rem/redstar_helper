c246dbd98b92730b52ae466a981e7e97c847ced3
Taylor Remund <taylorremund@Admins-MacBook-Pro.local>
2023-07-17 11:17:21 -0600

    removed create_folder.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,6 +1,30 @@
 import pandas as pd
 
-from create_folder import file_path
+from datetime import datetime
+import os, glob
+
+now = datetime.now()
+day = now.day
+month = now.month
+year = now.year
+username = "taylorremund"
+
+path = f"/Users/{username}/Desktop/red_star_helper/{year}/{month}/{day}"
+
+
+def get_csv_filename(path):
+    csv_files = glob.glob(os.path.join(path, "*.csv"))
+    if len(csv_files) == 0:
+        print(f"No CSV files found in the directory: {path}")
+        return None
+    elif len(csv_files) > 1:
+        print(f"Multiple CSV files found in the directory: {path}")
+        return None
+    else:
+        return os.path.basename(csv_files[0])
+
+
+file_path = f"{path}/{get_csv_filename(path)}"
 
 df = pd.read_csv(file_path)
 
